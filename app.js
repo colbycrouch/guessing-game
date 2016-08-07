50f57e9382740181d61ad8b4246a97151e996a0e
Colby Crouch <colbycrouch@gmail.com>
2016-08-07 14:09:52 -0700

    cleaned up some saves from code review before opening new branch for each file to finish page

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 var userName = prompt('Hey user, What\'s your name?');
 
 alert('Welcome to my guessing game ' + userName + '. Answer question numbers 1 thru 4 with \'y\' or \'n\'');
-function questionOne(){
+function questionOne() {
   var answerOne = prompt('Q.1) Does Colby like to cook?');
   answerOne = answerOne.toUpperCase();
   console.log('this is the first value of question ' + answerOne);
   if(answerOne === 'Y') {
     console.log('this is the if');
     alert('You just won the super bowl ' + userName + '!....wait no, You got that right!');
-  }else {
+  } else {
     console.log('this is the else');
     alert('You Suck!!! ' + userName + ' You got that wrong!');
   }
@@ -16,8 +16,8 @@ function questionOne(){
 questionOne();
 
 function questionTwo() {
-  answerTwo = prompt('Q.2) Does he drive an American Car?') .toUpperCase();
-  console.log ('this is the second value of question ' + answerTwo);
+  answerTwo = prompt('Q.2) Does he drive an American Car?').toUpperCase();
+  console.log('this is the second value of question ' + answerTwo);
   if (answerTwo === 'Y') {
     alert('You must know me.... ' + userName);
   } else {
@@ -27,46 +27,48 @@ function questionTwo() {
 questionTwo();
 
 function questionThree() {
-  var answerThree = prompt('Q.3) Does he prefer Tacos over Pizza?') .toUpperCase();
-  console.log ('this is the third value of question ' + answerThree);
-  if (answerThree === 'Y'){
+  var answerThree = prompt('Q.3) Does he prefer Tacos over Pizza?').toUpperCase();
+  console.log('this is the third value of question ' + answerThree);
+  if (answerThree === 'Y') {
     alert('This is getting creepy ' + userName);
-  } else if(answerThree === 'N') {
+  } else if (answerThree === 'N') {
     alert('You should just quit now!!!');
   }
 }
 questionThree();
 
 function questionFour() {
-  var answerFour = prompt('Q.4) Is he going to change careers?') .toUpperCase();
-  console.log ('this is the fourth value of question ' + answerFour);
-  if (answerFour === 'Y'){
+  var answerFour = prompt('Q.4) Is he going to change careers?').toUpperCase();
+  console.log('this is the fourth value of question ' + answerFour);
+  if (answerFour === 'Y') {
     alert('You are so right ' + userName);
-  } else if(answerFour === 'N') {
+  } else if (answerFour === 'N') {
     alert('You should have quit while you were ahead!!');
   }
 }
 questionFour();
-//
 
-// alert('Questions 5 thru 8 will require a one word or numerical answer');
-function questionFive(){
+alert('Questions 5 thru 8 will require a one word or numerical answer');
+function questionFive() {
   var answerFive = prompt('Q.5) Can you guess one of my favorite cars?').toLowerCase();
   console.log('this is the fifth value of question ' + answerFive);
   if (answerFive === 'camaro' || answerFive === 'trailblazer' || answerFive === 'corvette') {
-    return alert('Hot Diggity Dog, you must be psychic ' + userName);
-  }else {
-    return alert('That\'s ok, maybe next time you will be a better mind reader!');
+    alert('Hot Diggity Dog, you must be psychic ' + userName);
+  } else {
+    alert('That\'s ok, maybe next time you will be a better mind reader!');
   }
 }
 questionFive();
 
-function questionSix(){
+function questionSix() {
   var answerSix = prompt('Q.6) Guess a sport i enjoy watching...').toLowerCase();
   var sport = ['f1', 'mma', 'football', 'baseball'];
   console.log('this is the sixth value of question ' + answerSix);
   if (sport.indexOf(answerSix) >= 0) {
-    alert('Good Game, Play Again ' + userName + '?');
+    var playAgain = prompt('Good Game, Play Again ' + userName + '?').toLowerCase();
+    if (playAgain === 'y') {
+      questionSix();
+    }
   } else {
     alert('You are not right!');
   }
@@ -75,9 +77,12 @@ questionSix();
 
 function questionSeven(){
   var answerSeven = parseInt(prompt('Q.7) Can you guess my favorite number? You only have 5 chances'));
-  var answer = 71;
+  var answer = (71);
+  console.log('this is the answer', answer);
   var guessCounter = 1;
-  console.log('this is the seventh value of question' + answerSeven);
+  console.log('this is the seventh value of question ' + answerSeven);
+
+// Math.floor (Math.random() * 100
 
   while (answerSeven !== answer && guessCounter < 5) {
     if (isNaN(answerSeven)) {
@@ -101,14 +106,14 @@ function questionSeven(){
 }
 questionSeven();
 
-function questionEight(){
-  var arr = ['pitbull', 'rottweiler', 'doberman', 'roman cane corso'];
-  var answerEight = prompt('Q8.) Last question, Quick name a favorite dog breed of mine.');
+function questionEight() {
+  var dogs = ['pitbull', 'rottweiler', 'doberman', 'roman cane corso'];
+  var answerEight = prompt('Q8.) Last question, Quick name a favorite dog breed of mine.').toLowerCase();
   var isTrue = false;
   console.log('this is the eigth value of question' + answerEight);
 
-  for (var i = 0; i < arr.length; i++) {
-    if (answerEight === arr[i]) {
+  for (var i = 0; i < dogs.length; i++) {
+    if (answerEight === dogs[i]) {
       isTrue = true;
       break;
     }
